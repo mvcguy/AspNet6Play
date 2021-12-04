8264c3283515fe4a6b554847892b8cf2df556435
mvcguy <me.shahidali@hotmail.com>
2021-12-04 19:38:07 +0100

    fix typo in the model

--- PlayWebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ PlayWebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -70,6 +70,7 @@ namespace PlayWebApp.Areas.Identity.Pages.Account.Manage
             [Display(Name = "Last name")]
             [StringLength(128, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
 
+            public string LastName { get; set; }
         }
 
         private async Task LoadAsync(IdentityUser user)
