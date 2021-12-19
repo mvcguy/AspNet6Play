0a3bcd71ec6b438ccfd2c1ca22e0c89244dc97a3
mvcguy <me.shahidali@hotmail.com>
2021-12-19 19:33:52 +0100

    add ids views for PKCE interactive flow

--- PlayConnectServer/AppConfig/Config.cs
+++ PlayConnectServer/AppConfig/Config.cs
@@ -1,32 +1,100 @@
-using Duende.IdentityServer.Models;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
 
 namespace PlayConnectServer.AppConfig
 {
     public static class Config
     {
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new List<ApiScope>
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
             {
-                new ApiScope("api1", "My API")
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        //
+        // used to verify the audience
+        // if you are getting invalid_audience error, then add your audience name here
+        //
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]{
+                new ApiResource("play.services.logistics:API", "Logsitics API")
+                {
+                    Scopes = { "catalog.fullaccess", "catalog.readonlyaccess" }
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+                new ApiScope("logistics.fullaccess"),
+                new ApiScope("logistics.readonlyaccess")
             };
 
         public static IEnumerable<Client> Clients =>
-            new List<Client>
+            new Client[]
             {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "scope1" }
+                },
+
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope2" }
+                },
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5030/signin-oidc" },
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5030/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                },
                 new Client
                 {
                     ClientId = "client",
-
                     // no interactive user, use the clientid/secret for authentication
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-
                     // secret for authentication
                     ClientSecrets =
                     {
                         // "K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols="
                         new Secret("secret".Sha256())
                     },
-
                     // scopes that client has access to
                     AllowedScopes = { "api1" },
                     ClientClaimsPrefix = "",
--- PlayConnectServer/Program.cs
+++ PlayConnectServer/Program.cs
@@ -1,20 +1,52 @@
 
+using IdentityServer4;
 using PlayConnectServer.AppConfig;
+using PlayConnectServer.Quickstart;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-var clients = Config.Clients;
-services.AddIdentityServer(options =>
+services.AddControllersWithViews();
+
+var builderX = services.AddIdentityServer(options =>
 {
-    
-}).AddInMemoryApiScopes(Config.ApiScopes)
-.AddInMemoryClients(clients);
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+
+    // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+    options.EmitStaticAudienceClaim = true;
+    //options.Cors.CorsPolicyName = "mg.services";
+
+
+}).AddTestUsers(TestUsers.Users);
+
+// in-memory, code config
+builderX.AddInMemoryIdentityResources(Config.IdentityResources);
+builderX.AddInMemoryApiScopes(Config.ApiScopes);
+builderX.AddInMemoryClients(Config.Clients);
+builderX.AddInMemoryApiResources(Config.ApiResources);
+
+// not recommended for production - you need to store your key material somewhere secure
+builderX.AddDeveloperSigningCredential();
+
+services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+        // register your IdentityServer with Google at https://console.developers.google.com
+        // enable the Google+ API
+        // set the redirect URI to https://localhost:5001/signin-google
+        options.ClientId = "copy client ID from Google here";
+        options.ClientSecret = "copy client secret from Google here";
+    });
 
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();
 app.UseIdentityServer();
-
+app.UseAuthorization();
 app.Run();
