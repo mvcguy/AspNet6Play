f2e7c2704f677606cafb2e9dfaa514b8d85f6603
mvcguy <me.shahidali@hotmail.com>
2022-01-03 10:14:41 +0100

    api for re ordering col - in progress

--- PlayWebApp/wwwroot/js/Services/grid-configure.js
+++ PlayWebApp/wwwroot/js/Services/grid-configure.js
@@ -1,7 +1,6 @@
 $.fn.gridConfigure = function () {
 
     var headers = this.find('.ds-col');
-    //console.log(headers);
     var _this = this;
 
     var colsList = $('.grid-config-cols');
@@ -29,7 +28,6 @@ $.fn.gridConfigure = function () {
         colsList.append(colsListItem);
 
         chk.on('click', function () {
-            // continue
             var $chk = $(this);
             var prop = $chk.attr('data-config-propname');
             if (!prop) return;
@@ -42,9 +40,9 @@ $.fn.gridConfigure = function () {
             var index = Array.from(col.parent('tr').children()).indexOf(col[0]);
             if (index < 0) return;
 
-            //var rows = $(_this).parents.find('th[data-th-propname=' + prop + ']').parents('tr');
             $.each(rows, function () {
-                var cols = $(this).find('td, th')[index];
+                var $row = $(this);
+                var cols = $row.find('td, th')[index];
 
                 if (!cols || cols.length <= 0) return;
 
@@ -54,18 +52,119 @@ $.fn.gridConfigure = function () {
                 else {
                     $(cols).hide();
                 }
-
             });
+        });
+    });
+
+}
+
+$.fn.enableColumnReordering = function () {
+    console.log('enabling column re-ordering feature...');
+    var $table = $(this);
+    var $header = $table.find('thead');
+    var cols = $header.find('th');
+    cols.css('cursor', 'pointer');
+    cols.css('user-select', 'none');
+
+    var colPos = 0;
+    var curCol = undefined;
+
+    cols.on('mousedown', function (e) {
+        var col = this;
+        curCol = 'x';
+        console.log(e);
+
+        var rows = $table.find('tr');
+        var index = Array.from($(col).parent('tr').children()).indexOf(col);
+        // console.log(index);
+        var ofs = $(col).offset();
+        var pos = $(col).position();
+        var x = e.pageX;
+        var y = e.pageY;
+
+        colPos = x;
+
+        console.log(ofs, pos, x, y);
+
+        $(col).css('cursor', 'move');
+
+        $.each(rows, function () {
+            var $row = $(this);
+            var cls = $row.find('td, th')[index];
+
+            if (!cls || cls.length <= 0) return;
+            $(cls).css('border', 'solid 1px gray');
+            $(cls).css('position', 'relative');
 
 
         });
     });
 
+    cols.on('mouseup', function () {
+        var col = this;
+        curCol = undefined;
 
-    var saveButton = $('#btnSaveGridConfig');
-    saveButton.on('click', function () {
-        var colsList = $('.grid-config-cols');
+        var rows = $table.find('tr');
+        var index = Array.from($(col).parent('tr').children()).indexOf(col);
+        // console.log(index);
+        $(col).css('cursor', 'pointer');
+        $.each(rows, function () {
+            var $row = $(this);
+            var cls = $row.find('td, th')[index];
+
+            if (!cls || cls.length <= 0) return;
+            $(cls).css('border', '');
+            $(cls).css('position', '');
+
+        });
     });
 
+    cols.on('mousemove', function (e) {
+        if (!curCol) return;
+        var col = this;
+        var curPos = e.pageX;
+        var diff = curPos - colPos;
+        var pos = $(col).position();
+        if (diff < 0) return;
+
+        $(col).css('border', '1px solid red').css('position', 'relative');
+        // $(col).css('display', 'block');
+        $(col).css('left', pos.left + diff + 'px');
+
+        if (diff < 20) return;
+
+
+        // console.log(e);
+        var rows = $table.find('tr');
+        var index = Array.from($(col).parent('tr').children()).indexOf(col);
+        // console.log(index);
+        $.each(rows, function () {
+            var $row = $(this);
+            var cls = $row.find('td, th')[index];
+
+
+            if (!cls || cls.length <= 0) return;
+            var $cls = $(cls);
+
+            //$cls.css('display', 'block');
+
+            var col0 = $row.find('td, th')[0];
+            var col1 = $row.find('td, th')[1];
+            //console.log(col0, col1);
+
+            //swap
+            var temp = col0;
+            col0 = col1;
+            col1 = temp;
+            $row.append(col0);
+            $row.append(col1);
+
+            $(col0).css('border', '');
+            $(col1).css('position', '');
+
+
+        });
+        curCol = undefined;
+    });
 
 }
